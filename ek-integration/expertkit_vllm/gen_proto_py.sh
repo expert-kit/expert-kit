9e0377a6d2ef53349fa25938f14520258f2727b7
Liu Chongpeng <liuchongpeng1999@hotmail.com>
2025-03-31 09:48:07 +0000

    feat: update proto for expertkit-vllm, need fix some bug

--- ek-integration/expertkit_vllm/gen_proto_py.sh
+++ ek-integration/expertkit_vllm/gen_proto_py.sh
@@ -1,6 +1,17 @@
+#!/usr/bin/sh
+
+PROTO_DIR_PATH="../../ek-proto"
+OUTPUT_DIR="./expertkit_vllm/pbpy"
+PACKAGE_PREFIX="expertkit_vllm.pbpy"
+
+# generate protobuff and grpc code
 python -m grpc_tools.protoc \
-    -Iexpertkit_vllm/pbpy:./proto \
-    --python_out=./expertkit_vllm/pbpy \
-    --pyi_out=./expertkit_vllm/pbpy \
-    --grpc_python_out=./expertkit_vllm/pbpy \
-    ./proto/expert.proto
+    -I$PROTO_DIR_PATH \
+    --python_out=$OUTPUT_DIR \
+    --pyi_out=$OUTPUT_DIR \
+    --grpc_python_out=$OUTPUT_DIR \
+    $(find $PROTO_DIR_PATH -name '*.proto')
+
+# fix import path
+find $OUTPUT_DIR -name "*_pb2*.py" -type f -exec sed -i "s/from ek\./from $PACKAGE_PREFIX.ek./g" {} \;
+find $OUTPUT_DIR -name "*_pb2*.py" -type f -exec sed -i "s/import ek\./import $PACKAGE_PREFIX.ek./g" {} \;
