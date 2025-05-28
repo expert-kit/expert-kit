5b293f11d4bc54ead8d61cb71f09641745c1d33b
Liu Hancheng <liuhancheng.cn@gmail.com>
2025-05-28 17:49:17 +0800

    feat: propagate span context using tower middleware

---
+++ ek-base/src/tracing/grpc.rs
@@ -0,0 +1,137 @@
+use http::{Request, Response, Uri};
+use opentelemetry::propagation::{Extractor, Injector};
+use std::{
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
+use tracing::{Level, field::Empty};
+use tracing_opentelemetry::OpenTelemetrySpanExt;
+
+use tonic::{body::Body, transport::Channel};
+use tower::Service;
+
+#[derive(Default, Debug, Clone)]
+pub struct GRPCClientLayer;
+
+#[derive(Debug, Clone)]
+pub struct OTelGrpcClientService {
+    inner: Channel,
+}
+
+impl Service<Request<Body>> for OTelGrpcClientService {
+    type Response = Response<Body>;
+    type Error = Box<dyn std::error::Error + Send + Sync>;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx).map_err(Into::into)
+    }
+
+    fn call(&mut self, req: Request<Body>) -> Self::Future {
+        let mut req = req;
+        let clone = self.inner.clone();
+        let mut inner = std::mem::replace(&mut self.inner, clone);
+        let span = span_from_req(&req);
+        let ctx = span.context();
+
+        let mut injector = HeaderInjector(req.headers_mut());
+        opentelemetry::global::get_text_map_propagator(|propagator| {
+            propagator.inject_context(&ctx, &mut injector);
+        });
+        Box::pin(async move {
+            let resp = inner.call(req).await?;
+            Ok(resp)
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct OTelGrpcServerMiddleware<S> {
+    inner: S,
+}
+type BoxFuture<'a, T> = Pin<Box<dyn std::future::Future<Output = T> + Send + 'a>>;
+
+impl<S, ReqBody, ResBody> Service<http::Request<ReqBody>> for OTelGrpcServerMiddleware<S>
+where
+    S: Service<http::Request<ReqBody>, Response = http::Response<ResBody>> + Clone + Send + 'static,
+    S::Future: Send + 'static,
+    ReqBody: Send + 'static,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: http::Request<ReqBody>) -> Self::Future {
+        let clone = self.inner.clone();
+        let mut inner = std::mem::replace(&mut self.inner, clone);
+        let span = {
+            let span = span_from_req(&req);
+            let extractor = HeaderExtractor(req.headers());
+            let ctx = opentelemetry::global::get_text_map_propagator(|propagator| {
+                propagator.extract(&extractor)
+            });
+            span.set_parent(ctx);
+            span
+        };
+
+        Box::pin(async move {
+            // Do extra async work here...
+            let _enter = span.enter();
+            let response = inner.call(req).await?;
+
+            Ok(response)
+        })
+    }
+}
+
+pub fn span_from_req<B>(req: &http::Request<B>) -> tracing::Span {
+    let (service, method) = extract_service_method(req.uri());
+    tracing::span!(
+        Level::TRACE,
+        "grpc.request",
+        otel.name = format!("{service}/{method}"),
+        otel.kind = ?opentelemetry::trace::SpanKind::Server,
+        otel.status_code = Empty, // to set on response
+        trace_id = Empty, // to set on response
+        request_id = Empty, // to set
+        exception.message = Empty, // to set on response
+    )
+}
+
+pub fn extract_service_method(uri: &Uri) -> (&str, &str) {
+    let path = uri.path();
+    let mut parts = path.split('/').filter(|x| !x.is_empty());
+    let service = parts.next().unwrap_or_default();
+    let method = parts.next().unwrap_or_default();
+    (service, method)
+}
+
+pub struct HeaderInjector<'a>(pub &'a mut http::HeaderMap);
+impl Injector for HeaderInjector<'_> {
+    fn set(&mut self, key: &str, value: String) {
+        if let Ok(name) = http::header::HeaderName::from_bytes(key.as_bytes()) {
+            if let Ok(val) = http::header::HeaderValue::from_str(&value) {
+                self.0.insert(name, val);
+            }
+        }
+    }
+}
+
+pub struct HeaderExtractor<'a>(pub &'a http::HeaderMap);
+
+impl Extractor for HeaderExtractor<'_> {
+    fn get(&self, key: &str) -> Option<&str> {
+        self.0.get(key).and_then(|value| value.to_str().ok())
+    }
+    fn keys(&self) -> Vec<&str> {
+        self.0
+            .keys()
+            .map(http::HeaderName::as_str)
+            .collect::<Vec<_>>()
+    }
+}
