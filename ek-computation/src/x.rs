a9614bb5397bbca2b73d86fdcef4757ec5769da2
liuyang <1844061236@buaa.edu.cn>
2025-09-11 19:32:53 +0800

    feat: add CI for rust check of test and clippy

--- ek-computation/src/x.rs
+++ ek-computation/src/x.rs
@@ -98,7 +98,11 @@ mod test {
 
     #[test]
     fn test_force_cuda() {
-        let _ = tch::Tensor::zeros([1, 2], (tch::Kind::Float, tch::Device::Cuda(0)));
+        if !tch::Cuda::is_available() {
+            println!("CUDA is not available");
+            return;
+        }
+        let _ = tch::Tensor::zeros(&[1, 2], (tch::Kind::Float, tch::Device::Cuda(0)));
         println!("Tensor on CUDA successfully created.");
     }
 }
